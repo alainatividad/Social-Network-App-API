1fdff22ac7e57186986dc758d51df5227e094594
AN <99376802+alainatividad@users.noreply.github.com>
2022-06-25 22:18:25 +1000

    Added update and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,11 +49,62 @@ const addFriend = async (req, res) => {
     }
 
     //else update user's friends array with the friendId
-    const updateUser = await User.updateOne(
+    const updateUser = await User.findOneandUpdate(
       { _id: req.params.userId },
       { $addToSet: { friends: req.params.friendId } },
       { runValidators: true, new: true }
     );
+
+    res.json(updateUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const modifyUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId });
+
+    //if user doesn't exist, don't continue and return error
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
+    }
+
+    //else update user's details
+    const updateUser = await User.updateOne(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+
+    res.json(updateUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId });
+
+    //if user doesn't exist, don't continue and return error
+    if (!user) {
+      return res.status(404).json({ message: "No user with that ID" });
+    }
+
+    // "extract" the thoughts into an array
+    const thoughts = await User.aggregate([
+      { $match: { _id: ObjectId(req.params.userId) } },
+      { $unwind: "$thoughts" },
+    ]);
+
+    // and use foreach to delete each thoughtId from the thought document
+    thoughts.forEach(async (thought) => {
+      await Thought.findOneAndDelete({ _id: thought.thoughts._id });
+    });
+
+    //and then delete the user entry
+    const updateUser = await User.deleteOne({ _id: req.params.userId });
 
     res.json(updateUser);
   } catch (error) {
@@ -66,4 +117,6 @@ module.exports = {
   getSingleUser,
   createUser,
   addFriend,
+  modifyUser,
+  deleteUser,
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,15 @@ const {
   getSingleUser,
   createUser,
   addFriend,
+  modifyUser,
+  deleteUser,
 } = require("../../controllers/userController");
 
 // /api/users
 router.route("/").get(getAllUsers).post(createUser);
 
 // /api/users/:userId
-router.route("/:userId").get(getSingleUser);
+router.route("/:userId").get(getSingleUser).put(modifyUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 router.route("/:userId/friends/:friendId").post(addFriend);
